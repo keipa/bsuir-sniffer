11de257085f26f7e5b79acc82beaa035db129b81
Nickolai Rovdo <nichogaus@gmail.com>
2017-05-10 00:19:09 +0300

    moving to 2015 sln file, translating tryings

--- resource.h
+++ resource.h
@@ -2,10 +2,11 @@
 #define RESOURCE_H
 
 #define ENG_LANG
+//#define RUS_LANG
 
 #ifdef ENG_LANG
 
-#define STR_ABOUT "Sniffer. Built 20/11/2009. Anton A. Petrov.\n"
+#define STR_ABOUT "Sniffer. Built May 9, 2017\n"
 #define STR_CANT_CREATE_SOCKET "Can't create raw socket. This program must be run under administrator!"
 #define STR_SOCKET "Socket"
 #define STR_HOSTNAME "Hostname"
@@ -20,7 +21,19 @@
 
 #endif
 #else
+
 #ifdef RUS_LANG 
+//#define STR_ABOUT "Sniffer. Built May 9, 2017\n"
+//#define STR_CANT_CREATE_SOCKET "Невозможно создать пустой сокет. Необходимо запустить программу от имени АДМИНИСТРАТОРА!"
+//#define STR_SOCKET "Сокет"
+//#define STR_HOSTNAME "Хост"
+//#define STR_HOST_IP "IP-адрес хоста"
+//#define STR_PROM_MODE "Неразборчивый режим"
+//#define STR_CONSOLE_OUTPUT "Вывод в консоль (y/n): "
+//#define STR_RESOLUTION "Пропускная способность (задержка в мс): "
+//#define STR_WATCH_HOST "Просмотр хоста : "
+//#define STR_NET_SERVER_STARTED "Net-сервер на порту 2000 развёрнут. Используй telnet для соединения.\n\n"
+//#define STR_END_OF_PROGRAM "Спасибо за использование программы .\nЛог файл сохранён.\nIP-датаграмм всего:"
 
 #endif
 
